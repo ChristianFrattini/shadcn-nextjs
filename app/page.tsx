e6393c50c8fb4ad022416ee8144655cc2f4586ca
Christian Frattini <chrifrat1@gmail.com>
2024-04-05 19:12:08 +0100

    product list

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,67 @@
 import Container from "@/components/ui/container";
 import { Button } from "@/components/ui/button";
 import { ShoppingBag } from "lucide-react";
+import ProductList from "@/components/ProductList";
 
 export default function Home() {
+  const products = [
+    {
+      id: "1",
+      category: "Camera",
+      name: "Sony FX3",
+      price: "$3,999.00",
+      images: ["/img/products/FX3.png"],
+    },
+    {
+      id: "2",
+      category: "Camera",
+      name: "Sony A7S III",
+      price: "$3,499.00",
+      images: ["/img/products/7SIII.png"],
+    },
+    {
+      id: "3",
+      category: "Camera",
+      name: "Sony A7C",
+      price: "$1,599.00",
+      images: ["/img/products/7C.png"],
+    },
+    {
+      id: "4",
+      category: "Camera",
+      name: "Sony A7 IV",
+      price: "$2,399.00",
+      images: ["/img/products/7IV.png"],
+    },
+    {
+      id: "5",
+      category: "Camera",
+      name: "Sony A7R III",
+      price: "$2,499.00",
+      images: ["/img/products/7RIII.png"],
+    },
+    {
+      id: "6",
+      category: "Camera",
+      name: "Sony A7R V",
+      price: "$3,899.00",
+      images: ["/img/products/7RV.png"],
+    },
+    {
+      id: "7",
+      category: "Camera",
+      name: "Sony A6700",
+      price: "$1,799.00",
+      images: ["/img/products/6700.png"],
+    },
+    {
+      id: "8",
+      category: "Camera",
+      name: "Sony AZV-E10",
+      price: "$699.00",
+      images: ["/img/products/ZVE10.png"],
+    },
+  ];
   return (
     <Container>
       <div className="space-y-10 pb-10">
@@ -22,7 +81,9 @@ export default function Home() {
             </div>
           </div>
         </div>
-        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8"></div>
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList items={products} />
+        </div>
       </div>
     </Container>
   );
